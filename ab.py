a6a27e57e469c9cdf641b53914feb672f59cc0e3
parth <parthjain.pj1994@gmail.com>
2018-04-27 16:25:59 -0700

    Degree, Avg pathlength,clusters, cluster coeeficient, number of nodes

--- ab.py
+++
@@ -1,9 +0,0 @@
-import igraph
-import os
-
-# print "hi"
-f = open("0301/.0modified.txt", 'r')
-g = igraph.Graph.Read_Ncol(f, directed=True)
-f.close()
-g.vs['label'] = [i for i in range(1,5659)]
-igraph.plot(g)
